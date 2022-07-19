050449e64f4981dcb415f854bcca183689c07874
Ragini.Pandey <pandeyragini55@gmail.com>
2022-07-19 11:23:02 +0530

    Initial changes

--- First.py
+++ First.py
@@ -1,1 +1,21 @@
-print("Hello world")
+#print("Hello world")
+
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+operator = input("Enter operator (+,-,*,/,%): ")
+num1 = int(num1)
+num2 = int(num2)
+#Mini project : Calculator
+
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator == "%":
+    print(num1 % num2)
+else:
+    print("Invalid Operation")
